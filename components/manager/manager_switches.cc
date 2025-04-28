8fc1c238c9443fdf6e2f969a20b4e2cbe8460cd1
Ahmad maulana yusuf <77580091+WiiAhmad@users.noreply.github.com>
2025-04-28 16:37:56 +0700

    Test: testing for custom flags

---
+++ components/manager/manager_switches.cc
@@ -0,0 +1,6 @@
+#include "components/manager/manager_switches.h"
+
+namespace switches {
+    const char kProfile[] = "profile";
+}
+
---
+++ components/manager/manager_switches.h
@@ -0,0 +1,10 @@
+#ifndef THIRD_PARTY_MANAGER_SWITCHES_H_
+#define THIRD_PARTY_MANAGER_SWITCHES_H_
+
+#include "base/component_export.h"
+
+namespace switches {
+    COMPONENT_EXPORT(MANAGER) extern const char kProfile[];
+}
+
+#endif
